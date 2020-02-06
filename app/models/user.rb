7667922a39e3870a4700041506837ebc9f0e2edf
Toyohiro Sasaya <toyohiro.sasaya@gmail.com>
2020-02-06 08:20:06 +0000

    rails_4

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/ 
+  validates :password, presence: true, length: {in: 8..32}, format: {with: VALID_PASSWORD_REGEX}
+  
+end
