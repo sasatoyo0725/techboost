9e2002be77766c98f5cfa97758f0793c69f26c62
Toyohiro Sasaya <toyohiro.sasaya@gmail.com>
2020-02-10 01:01:35 +0000

    rails_task_5_2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,5 @@ class ApplicationController < ActionController::Base
   
   add_flash_types :success, :info, :warining, :danger
   
-  helper_method :current_user, :logged_in?
-  
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def logged_in?
-    !current_user.nil?
-  end
+  include SessionsHelper
 end
