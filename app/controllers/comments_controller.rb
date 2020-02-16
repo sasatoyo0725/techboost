15401f6572fb13889bf63c06e3de5067a4f51b42
Toyohiro Sasaya <toyohiro.sasaya@gmail.com>
2020-02-16 20:20:43 +0000

    nice color comment 2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,6 @@ class CommentsController < ApplicationController
   private
   
   def comment_params
-    params.require(:comment).permit(:body, :comment, :topic_id, :user_id)
+    params.require(:comment).permit(:body, :comment, :topic_id)
   end
 end
